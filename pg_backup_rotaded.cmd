c59aad92bbab4d4a5ef41edb346ec2332cc2294b
João Batista da Silva Avila <42388872+joaoavilars@users.noreply.github.com>
2022-09-02 16:19:15 -0300

    Update pg_backup_rotaded.cmd
    update order of functions

--- pg_backup_rotaded.cmd
+++ pg_backup_rotaded.cmd
@@ -12,45 +12,35 @@ set diaatual=%%c-%%b-%%a
 set dia=%%a
 set mes=%%b
 set ano=%%c)
+
+
+
 :::::::::::::::::::::::::::
 :::: START THE BACKUPS ::::
 ::::::::::::::::::::::::::::
 :: get day of week number
 for /f %%i in ('powershell ^(get-date^).dayOfWeek.value__') do set DAY_OF_WEEK=%%i
 if %DAY_OF_WEEK% == %DAY_OF_WEEK_TO_KEEP% (goto WEEK)
+
 ::get day of the month
 if %dia% == 01 (goto MONTHLY)
+
 :: DAILY BACKUP
-:: na linha de comando deve-se colocar pg_backup_rotaded.cmd daily 
 set SUFFIX=daily
 set FINAL_BACKUP_DIR=%BACKUP_DIR%\%diaatual%-%SUFFIX%
 IF NOT EXIST %FINAL_BACKUP_DIR% (mkdir %FINAL_BACKUP_DIR%)
- 
-::::::::::::::::::::::::
-:::: GLOBALS BACKUPS ::::
-::::::::::::::::::::::::
-IF %ENABLE_GLOBALS_BACKUPS% == "yes" (
-pg_dumpall -g -U %USERNAME% -f %FINAL_BACKUP_DIR%\globals.sql %DATABASE%)
- 
-::::::::::::::::::::::::::::
-:::::: FULL BACKUPS ::::::::
-::::::::::::::::::::::::::::
-if %ENABLE_PLAIN_BACKUPS% == "yes" (
-pg_dump --host=%HOSTNAME% -U %USERNAME% --format=%PLAIN_BACKUPS_YES% %other_pg_dump_flags% -f %FINAL_BACKUP_DIR%\%DATABASE%_PLAIN.sql %DATABASE%)
 
-if %ENABLE_CUSTOM_BACKUPS% == "yes" (
-pg_dump --host=%HOSTNAME% -U %USERNAME% --format=%file_format% %other_pg_dump_flags% -f %FINAL_BACKUP_DIR%\%DATABASE%.sql %DATABASE%)
 
- 
-:: MONTHLY BACKUPS
+
 :MONTHLY
+:: MONTHLY BACKUPS
 if %dia% == 01 (
 FORFILES /P %BACKUP_DIR% /M *-monthly /C "cmd /c IF @isdir == TRUE rd /S /Q @path"
 set SUFFIX=monthly
 set FINAL_BACKUP_DIR=%BACKUP_DIR%\%diaatual%-!SUFFIX!
 IF EXIST !FINAL_BACKUP_DIR!\%DATABASE%.sql (goto EOF)
 IF NOT EXIST !FINAL_BACKUP_DIR! (mkdir !FINAL_BACKUP_DIR!)
-pg_dump --host=%HOSTNAME% -U %USERNAME% --format=%file_format% %other_pg_dump_flags% -f !FINAL_BACKUP_DIR!\%DATABASE%.sql %DATABASE%
+pg_dump --host=%HOSTNAME% --port=%DBPORT% --username=%USERDB% --format=%file_format% %other_pg_dump_flags% -f !FINAL_BACKUP_DIR!\%DATABASE%.sql %DATABASE%
 if %DAY_OF_WEEK% == %DAY_OF_WEEK_TO_KEEP% (goto WEEK)
 goto EOF)
 
@@ -65,7 +55,7 @@ FORFILES /P %BACKUP_DIR% /D -%EXPIRED_DAYS% /M *-!SUFFIX! /C "cmd /c IF @isdir =
 set FINAL_BACKUP_DIR=%BACKUP_DIR%\%diaatual%-!SUFFIX!
 IF EXIST !FINAL_BACKUP_DIR!\%DATABASE%.sql (goto EOF)
 IF NOT EXIST !FINAL_BACKUP_DIR! (mkdir !FINAL_BACKUP_DIR!)
-pg_dump --host=%HOSTNAME% -U %USERNAME% --format=%file_format% %other_pg_dump_flags% -f !FINAL_BACKUP_DIR!\%DATABASE%.sql %DATABASE%
+pg_dump --host=%HOSTNAME%  --port=%DBPORT% --username=%USERDB% --format=%file_format% %other_pg_dump_flags% -f !FINAL_BACKUP_DIR!\%DATABASE%.sql %DATABASE%
 if %dia% == 01 (goto MONTHLY)
 goto EOF)
  
@@ -76,7 +66,25 @@ set SUFFIX=daily
 FORFILES /P %BACKUP_DIR% /D -%DAYS_TO_KEEP% /M *-%SUFFIX% /C "cmd /c IF @isdir == TRUE rd /S /Q @path"
 set FINAL_BACKUP_DIR=%BACKUP_DIR%\%diaatual%-%SUFFIX%
 IF NOT EXIST %FINAL_BACKUP_DIR% (mkdir %FINAL_BACKUP_DIR%)
-pg_dump --host=%HOSTNAME% -U %USERNAME% --format=%file_format% %other_pg_dump_flags% -f %FINAL_BACKUP_DIR%\%DATABASE%.sql %DATABASE%
+pg_dump --host=%HOSTNAME%  --port=%DBPORT% --username=%USERDB% --format=%file_format% %other_pg_dump_flags% -f %FINAL_BACKUP_DIR%\%DATABASE%.sql %DATABASE%
 
 :EOF
+
+::::::::::::::::::::::::
+:::: GLOBALS BACKUPS ::::
+::::::::::::::::::::::::
+IF %ENABLE_GLOBALS_BACKUPS% == "yes" (
+pg_dumpall -g --username=%USERDB% -f %FINAL_BACKUP_DIR%\globals.sql %DATABASE%)
+ 
+::::::::::::::::::::::::::::
+:::::: FULL BACKUPS ::::::::
+::::::::::::::::::::::::::::
+if %ENABLE_PLAIN_BACKUPS% == "yes" (
+pg_dump --host=%HOSTNAME%  --port=%DBPORT% --username=%USERDB% --format=%PLAIN_BACKUPS_YES% %other_pg_dump_flags% -f %FINAL_BACKUP_DIR%\%DATABASE%_PLAIN.sql %DATABASE%)
+
+if %ENABLE_CUSTOM_BACKUPS% == "yes" (
+pg_dump --host=%HOSTNAME%  --port=%DBPORT% --username=%USERDB% --format=%file_format% %other_pg_dump_flags% -f %FINAL_BACKUP_DIR%\%DATABASE%.sql %DATABASE%)
+
+
+
 ECHO SCRIPT FINALIZADO
